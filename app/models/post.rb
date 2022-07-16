a01b3480f993a656d145016aa1f42b6f18894d41
Anucet Murhula <anicetmurhula20@gmail.com>
2022-07-16 19:47:18 +0200

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
+  validates :title, presence: true, length: { maximum: 250, too_long: 'Title must not exceed 250 characters' }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_posts_counter
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: { message: 'Name field can not left blank' }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   private
 
   def most_recent_posts
