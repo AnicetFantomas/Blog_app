6fb52a3a3ab768aa835496f5d89093bd110c6871
Anucet Murhula <anicetmurhula20@gmail.com>
2022-07-23 14:01:30 +0200

    Update posts controller methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,69 +1,30 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[show edit update destroy]
-
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show; end
-
-  # GET /comments/new
   def new
     @comment = Comment.new
+    @user = current_user
   end
 
-  # GET /comments/1/edit
-  def edit; end
-
-  # POST /comments or /comments.json
   def create
+    puts "comment params: #{comment_params}"
+    @post = Post.find(params[:post_id] || params[:id])
     @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    @comment.post = @post
 
     respond_to do |format|
-      if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+      if @comment.save!
+        format.html do
+          redirect_to user_post_url(current_user, @post), notice: 'Comment was successfully created.'
+        end
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1 or /comments/1.json
-  def destroy
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:Text)
+    params.require(:comment).permit(:text)
   end
 end
