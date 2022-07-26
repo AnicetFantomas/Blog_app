a34ef33476dccb72439fef528f0d44d2428934b6
Anucet Murhula <anicetmurhula20@gmail.com>
2022-07-26 16:04:27 +0200

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,23 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  belongs_to :user, class_name: 'User',
+                    foreign_key: 'author_id',
+                    counter_cache: :posts_counter
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-  belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: 'post_id'
-  has_many :likes, foreign_key: 'post_id'
-
-  after_save :update_posts_counter
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+  after_save :update_posts_count
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  def most_recent_five_comments
+    comments.includes(:user).order(created_at: :desc).limit(5)
   end
 
   private
 
-  def update_posts_counter
-    author.increment!(:posts_counter)
+  def update_posts_count
+    user.update(posts_counter: user.posts.size)
   end
 end
