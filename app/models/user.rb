d4aa4693a4ff335b31eff996836a9e252094abdb
Anucet Murhula <anicetmurhula20@gmail.com>
2022-07-26 16:02:10 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
-  validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
-
-  def recent_posts
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  validates :posts_counter, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  validates :name, presence: true
+  def most_recent_three_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
