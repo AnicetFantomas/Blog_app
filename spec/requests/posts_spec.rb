2b964ad95e66d59fe751a4a411b372f477941500
Anucet Murhula <anicetmurhula20@gmail.com>
2022-07-27 14:39:57 +0200

    Update failing tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,36 +1,35 @@
-require 'rails_helper'
-
-RSpec.describe 'Posts', type: :request do
-  before { get user_posts_path(user_id: User.first.id) }
-  describe 'GET /posts' do
-    before { user_posts_path(1) }
-
-    it 'returns correct status code' do
-      expect(response).to have_http_status(200)
-    end
-
-    it 'Should render index template' do
-      expect(response).to render_template(:index)
-    end
-
-    it 'Should have text Index Posts' do
-      expect(response.body).to include('Here is a list of posts for a given user')
+RSpec.describe 'Users Controller', type: :request do
+  describe 'GET /index' do
+    context 'when the page is loaded' do
+      before do
+        get '/users'
+      end
+      it 'returns a 200 status code' do
+        expect(response).to have_http_status(200)
+      end
+      it 'renders the index template' do
+        expect(response).to render_template('index')
+        expect(response.body).to render_template('index')
+      end
     end
   end
 
-  describe 'GET /posts/:id' do
-    before { get user_post_path(1, 1) }
-    it 'returns correct status code' do
-      get user_post_path(User.first.id, id: Post.first.id)
-      expect(response).to have_http_status(200)
-    end
-
-    it 'Should render show template' do
-      expect(response).to render_template(:show)
-    end
-
-    it 'Should have text Show Post' do
-      expect(response.body).to include('Here is a specific post')
+  describe 'GET /show' do
+    let!(:user) { User.create(name: 'Monica', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.', posts_counter: 0) }
+    context 'when the page is loaded' do
+      before do
+        get user_path(user.id)
+      end
+      it 'returns a 200 status code' do
+        expect(response).to have_http_status(200)
+      end
+      it 'renders the show template' do
+        expect(response).to render_template('show')
+        expect(response.body).to render_template('show')
+      end
+      it 'includes see all posts' do
+        expect(response.body).to include('See all posts')
+      end
     end
   end
 end
