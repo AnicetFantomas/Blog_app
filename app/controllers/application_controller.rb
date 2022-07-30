68f15794a098a986805d1191651e1a17408e1478
Anucet Murhula <anicetmurhula20@gmail.com>
2022-07-30 10:23:50 +0200

    Update name for confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password posts_counter])
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |field|
+      field.permit(:name, :photo, :bio, :posts_counter, :email, :password, :password_confirmation)
+    end
   end
 end
+
